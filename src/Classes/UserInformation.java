156aa1711f6d51effe3cf8618d36dbe200ba378a
Mateaus <mateausc@gmail.com>
2018-08-16 05:47:49 -0500

    new class UserInformation to set/get information

---
+++ src/Classes/UserInformation.java
@@ -0,0 +1,31 @@
+package Classes;
+
+public class UserInformation {
+
+    private String name, password;
+
+    public UserInformation() {
+        return;
+    }
+
+    public UserInformation(String name, String password){
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
