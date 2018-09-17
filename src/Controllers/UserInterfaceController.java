1892fa5b74de535477add7c6de7c90a8272dc0b3
Mateaus <mateausc@gmail.com>
2018-09-16 20:48:04 -0500

    added thread to refresh UI scene

--- src/Controllers/UserInterfaceController.java
+++ src/Controllers/UserInterfaceController.java
@@ -5,6 +5,8 @@ import Database.DatabaseStatus;
 import HttpRequests.ActivityRequest;
 import HttpRequests.HttpHandler;
 import HttpRequests.StatusRequest;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -22,6 +24,8 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class UserInterfaceController implements Initializable {
 
@@ -31,13 +35,15 @@ public class UserInterfaceController implements Initializable {
     @FXML private Label studentsnmLB;
     @FXML private Button logoutBtn;
     private String studentId;
+    private volatile boolean isRunning = true;
+
+    Timer t = new Timer();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // We start reading from here
+        Boolean flag = true;
         studentLB.setText(Context.getInstance().currentUserInformation().getStudentName());
-        System.out.println(Context.getInstance().currentUserInformation().getStudentId());
-        System.out.println(Context.getInstance().currentUserInformation().getStudentName());
 
         try {
             String studentId = Context.getInstance().currentUserInformation().getStudentId();
@@ -64,10 +70,48 @@ public class UserInterfaceController implements Initializable {
         } catch (Exception e) {
             System.out.println(e);
         }
+        // TODO: Write a cleaner thread and make it stop when not being used.
+        Task task = new Task<Void>() {
+            @Override
+            public Void call() throws Exception {
+                int i = 0;
+                while (isRunning) {
+                    final int finalI = i;
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                StatusRequest statusRequest = new StatusRequest("online");
+                                HttpHandler httpHandler = new HttpHandler(statusRequest.getStatusRequestUrl(), statusRequest.getValuePairs());
+                                HttpResponse httpResponse = httpHandler.HttpResponseRequest(httpHandler.HttpPostRequest());
+                                String json = EntityUtils.toString(httpResponse.getEntity());
+                                JSONObject jsonObject = new JSONObject(json);
+                                int studentsAvailable = Integer.parseInt(jsonObject.getJSONObject("available").get("number").toString());
+                                String studentNames = "";
+                                for (int i = 0; i < studentsAvailable; i++) {
+                                    studentNames = studentNames + "-" + jsonObject.getJSONObject("student"+i).get("name").toString() + "\n";
+                                }
+                                System.out.println(studentNames);
+                                studentsnmLB.setText(studentNames);
+                            } catch (Exception e) {
+                                System.out.println(e);
+                            }
+                        }
+                    });
+                    i++;
+                    Thread.sleep(10000);
+                }
+                return null;
+            }
+        };
+        Thread th = new Thread(task);
+        th.setDaemon(true);
+        th.start();
 
         logoutBtn.setOnAction(
                 event -> {
                     try {
+                        this.isRunning = false;
                         String studentId = Context.getInstance().currentUserInformation().getStudentId();
                         ActivityRequest activityRequest = new ActivityRequest(studentId, "offline");
                         HttpHandler httpHandler = new HttpHandler(activityRequest.getActivityRequestUrl(), activityRequest.getValuePairs());
