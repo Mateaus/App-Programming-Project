bf2fd98fa5c77f04324953863a9a3ff70a035d62
lndill06 <43283879+lndill06@users.noreply.github.com>
2018-09-19 18:38:44 -0500

    Added password check, error messages
    Checks if password field is the same as retype password field and displays registration error messages to the registration scene.

--- src/Controllers/RegistrationController.java
+++ src/Controllers/RegistrationController.java
@@ -5,7 +5,9 @@ import HttpRequests.RegisterRequest;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
 import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
 
@@ -16,27 +18,36 @@ import java.util.ResourceBundle;
 public class RegistrationController implements Initializable {
 
     @FXML
-    private TextField nameTF, usernameTF, passTF;
+    private TextField nameTF, usernameTF, passTF, passCheckTF;
+    
+    @FXML
+    private Label errorMessage;
 
     public void initialize(URL location, ResourceBundle resources) {
-
+    	
     }
 
     public void registerUser(ActionEvent event) {
         String name = nameTF.getText().toString();
         String username = usernameTF.getText().toString();
         String password = passTF.getText().toString();
+        String passwordCheck = passCheckTF.getText().toString();
         try {
             if(nameTF != null && !name.isEmpty() && usernameTF != null && !username.isEmpty()
-                    && passTF != null && !password.isEmpty()) {
+                    && passTF != null && !password.isEmpty() && password.equals(passwordCheck)) {
                 RegisterRequest registerRequest = new RegisterRequest(name, username, password);
                 HttpHandler httpHandler = new HttpHandler(registerRequest.getRegisterRequestUrl(), registerRequest.getValuePairs());
                 HttpResponse httpResponse = httpHandler.HttpResponseRequest(httpHandler.HttpPostRequest());
                 EntityUtils.consume(httpResponse.getEntity());
                 LoginController loginController = new LoginController();
                 loginController.changeToMainScreen(event);
+            } else if (nameTF != null && !name.isEmpty() && usernameTF != null && !username.isEmpty()
+                    && passTF != null && !password.isEmpty() && !password.equals(passwordCheck)) {
+            	String passException = "Passwords do not match";
+            	errorMessage.setText(passException);
             } else {
-                System.out.printf("Fill the blanks\n");
+            	String fillException = "Fill the blanks";
+            	errorMessage.setText(fillException);
             }
         } catch (Exception e) {
             System.out.printf("Account not created\n" + e);
